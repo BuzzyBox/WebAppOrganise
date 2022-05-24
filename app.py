223557b01ad3d5b2d3f96fad42dbfe4c63a38850
Tam Hug <270007579@yoobeestudent.ac.nz>
2022-05-24 22:56:17 +1200

    Updated the login page.
    I have updated the login page to be more responsive. Still needs work.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app=Flask(__name__)
 
@@ -8,3 +8,13 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('style.html', error=error)
