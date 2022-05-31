9487cdaf68fe635d498058befcea5c6c07eb8740
BuzzyBox <270121466@yoobeestudent.ac.nz>
2022-06-01 01:00:37 +1200

    Update on Calendar 3
    Sorry there are a lot of changes at the moment but I am slowly getting there though, I'll make sure this is up and working soon!

--- CalendarPage/calendarWebPage.py
+++ CalendarPage/calendarWebPage.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+events = [
+    {
+        'todo': 'EventTest',
+        'date': '2022-06-01',
+    },
+    {
+        'todo': 'EventTest2',
+        'date': '2022-06-02',
+    }
+]
 
 @app.route('/')
 def introduction():
@@ -9,7 +19,7 @@ def introduction():
 
 @app.route('/mycalendar')
 def index():
-    return render_template('calendarWebPage.html')
+    return render_template('calendarWebPage.html', events = events)
 
 
 if __name__ == '__main__':
