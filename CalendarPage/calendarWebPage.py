11a00800ae8d1c44529722693469081122413f8f
BuzzyBox <85328835+BuzzyBox@users.noreply.github.com>
2022-06-02 11:32:05 +1200

    Update on Calendar 6
    This is an old update from my class computer, so it is a bit weird compare to the other update, anyway I am working on an edited click on event and so far it shows alerts but I will hopefully change that tonight!

--- CalendarPage/calendarWebPage.py
+++ CalendarPage/calendarWebPage.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+"""
+events = [
+    {
+        'todo': 'EventTest',
+        'date': '2022-06-01',
+    },
+    {
+        'todo': 'EventTest2',
+        'date': '2022-06-02',
+    }
+]"""
+
+@app.route('/')
+def introduction():
+    return "Hello World"
+
+
 @app.route('/mycalendar')
 def index():
-    return render_template('calendarWebPage.html')
+    return render_template('calendarWebPage.html')  #, events = events
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
