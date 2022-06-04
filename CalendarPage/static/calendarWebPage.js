ac9120eb3c53874e610466dcab33f5b834923c29
BuzzyBox <270121466@yoobeestudent.ac.nz>
2022-06-04 17:58:27 +1200

    Update on Calendar 10
    Not much of an update but I am quickly trying to save notes on here so I don't confuse moving into a different area

--- CalendarPage/static/calendarWebPage.js
+++ CalendarPage/static/calendarWebPage.js
@@ -1,3 +1,151 @@
+
+        function addListeners()
+        {
+            document.getElementById("event-modal-close-btn").addEventListener("click", closeEditModalBox, false);
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+         var Calendar = FullCalendar.Calendar;
+         var Draggable = FullCalendar.Draggable;
+
+        let containerEl = document.getElementById('external-event');
+        let calendarEl = document.getElementById('calendar');
+
+        let calendar = new FullCalendar.Calendar(calendarEl, {
+         headerToolbar:{
+        right: 'today,prev,next',
+        center: 'title',
+        left: 'dayGridMonth,timeGridWeek'
+         },
+
+         // initialView: 'timeGridWeek',
+          droppable : true,
+          editable  : true,
+          selectable: true,
+
+          events: [],
+
+          /*event click is going to work well here now I need to try and figure out if it's going to work with user
+          input and flask */
+
+          /* okay so I will need to edited eventClick so it can work to change the title and uploading files rather
+          than alerts if I can */
+
+          /* I might even look at wtfroms and or wtf flask for this area */
+
+          eventClick: function(info) {
+
+            alert('Event: ' + info.event.title);
+            alert('Date: ' + info.event.start);
+            alert('Date: ' + info.event.end);
+            //alert('View: ' + info.view.type);
+            alert("CLICKED!");
+           // toEditItem(info.event);
+
+
+
+             //document.getElementById('start').value = info.dateStr;
+            // document.getElementById('title').textContent =  "Register the Event";
+            //  myForm.show();
+
+
+
+            /* need to figure out where info.el.style is within css because the blue border on the events looks bad */
+            info.el.style.borderColor = 'pink';
+            //  event.backgroundColor = 'red';
+          }
+        });
+        calendar.render();
+
+
+    new Draggable(containerEl, {
+        itemSelector: '.fc-event',
+        eventData: function(eventEl) {
+        return {
+            title: eventEl.innerText
+                };
+        }
+    });
+
+    function toEditItem(event)
+    {   showEditModelBox();
+        let id;
+
+        if(event.target.dataset)
+        id = event.target.dataset.id;
+        else
+        id = event.id;
+        console.log(id);
+
+        preFillEditForm(id);
+    }
+/*function preFillEditForm(id)
+    {
+        let result = todoList.find(todoObj => todoObj.id == id);
+                let {title, start, end} = results;
+
+                document.getElementById("title").value = title;
+                document.getElementById("start").value = start;
+                document.getElementById("end").value = end;
+
+                changeBtn.dataset.id = id;
+    }*/
+
+    function buttonTest()
+    {
+        //var element = document.getElementById("edit-events");
+        //element.classList.toggle("slideIntoView");
+    }
+
+    function closeEditModalBox(event)
+    {
+       // document.getElementById("edit-events").classList.remove("slideIntoView");
+    }
+
+
+   });
+  function buttonTest()
+    {
+     /*the problem was that the script was not located within the body area of the work and-or the function was
+      inside the function for the calendar that could be it as well! */
+
+       var open = document.getElementById("edit-events");
+       open.classList.toggle("slideIntoView");
+    }
+function myButton()
+    {
+     var element = document.getElementById("myDIV");
+     element.classList.toggle("style-test");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*function events()
 {
 var Draggable = FullCalendar.Draggable;
