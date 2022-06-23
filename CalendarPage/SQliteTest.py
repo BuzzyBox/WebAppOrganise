4434926e2c3dde16f9d930e3813cb08af7d8144b
BuzzyBox <270121466@yoobeestudent.ac.nz>
2022-06-23 18:48:20 +1200

    Update on Calendar 15
    Getting close to the final product. Made huge changes towards these files, such as making the switch from java towards jQuery and saving the data into MySQL. I am currently saving them into different .py and .hmtl files right now but they should be updating into the main calendarWebPage files very soon!

---
+++ CalendarPage/SQliteTest.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL, MySQLdb
+
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crud2'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM events ORDER BY id")
+    calendar = cur.fetchall()
+    return render_template('SQliteTest.html', calendar=calendar)
+
+
+@app.route('/inset', methods=['POST'])
+def insert():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+        flash("Data Inserted Successfully!")
+        title = request.form['title']
+        start = request.form['start']
+        end = request.form['end']
+        print(title)
+        print(start)
+        cur.execute("INSERT INTO events (title,start_event,end_event) VALUES (%s,%s,%s)", [title, start, end])
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+
+
+@app.route("/update", methods=["POST", "GET"])
+def update():
+    cursor = mysql.connection.cursor()
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == 'POST':
+
+        title = request.form['title']
+        start = request.form['start']
+        end = request.form['end']
+        id = request.form['id']
+        print(title)
+        print(start)
+        cur.execute("UPDATE events SET title = %s, start_event = %s, end_event = %s WHERE id = %s",
+                    [title, start, end, id])
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+
+
+@app.route("/ajax_delete/<string:id>", methods=["GET", "POST"])
+def ajax_delete(id):
+
+    flash("Record has been Deleted Successfully")
+    con = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    con.execute("DELETE FROM events WHERE id = %s" % (id))
+    mysql.connection.commit()
+    con.close()
+    return redirect(url_for('index'))
+
+
+
+    # cursor = mysql.connection.cursor()
+    # cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # if request.method == 'POST':
+    #     cur.execute('DELETE FROM events WHERE id = %s' % (id_data))
+    #     mysql.connection.commit()
+    #     cur.close()
+    #     flash("Record has been Deleted Successfully")
+    #     return redirect(url_for(index))
+
+
+
+    # cursor = mysql.connection.cursor()
+    # cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # if request.method == 'POST':
+    #     flash("Record has been Deleted Successfully")
+    #     getid = request.form['string']
+    #     print(getid)
+    #     cur.execute('DELETE FROM events WHERE id = {0}'.format(getid))
+    #     mysql.connection.commit()
+    #     cur.close()
+    #
+    #     return redirect(url_for('index'))
+    #     msg = 'Record deleted successfully'
+    # return jsonify(msg)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
